892133aeff03b94c841192303f96a892089123b0
nikmodem <91239189+nikmodem@users.noreply.github.com>
2024-02-10 14:38:36 +1000

    Create lab1.c

---
+++ lab1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h> 
+
+int main()
+{
+  double a=1, b=2, c=3;
+  double x1, x2, m = 0;
+  n = pow(n,2)-4*a*c;
+  printf("n = %if\n",m);
+  x1 = (-m+sqrt(m))/(2*a);
+  x2 = (-m-sqrt(m))/(2*a);
+  printf("x1 = %if x2 = %if",x1,x2);
+  return 0;
+}
