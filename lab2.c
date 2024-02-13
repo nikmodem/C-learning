8b4ea0f5ee4e2289620bd4762a91329d850a57f3
nikmodem <91239189+nikmodem@users.noreply.github.com>
2024-02-13 19:07:06 +1000

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
-#include <math.h> 
+#include <stdlib.h>
+#include <math.h>
 
 int main()
 {
-  
+    double a, b, c, D=0, x1, x2;
+    
+    a = 1;
+    b = 3;
+    c = 1;
+    D = b*b - 4*a*c;
+    printf("D = %lf", D);
